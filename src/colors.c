ae564c69fb70dc7ddea041c8903896b3e5b11cf2
Mohammad abir Abbas <mabbas@c2s13d65.42wolfsburg.de>
2022-12-10 23:40:06 +0100

    Fractol final

--- src/colors.c
+++ src/colors.c
@@ -6,7 +6,7 @@
 /*   By: mabbas <mabbas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 14:30:35 by mabbas            #+#    #+#             */
-/*   Updated: 2022/12/10 00:34:28 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/12/10 23:39:41 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,3 +63,32 @@ int	color_init(t_mlx *mlx)
 	}
 	return (trgb_gen(color[0], color[1], color[2], color[3]));
 }
+
+/** Experimental Color conversions **/
+// int	color_init(t_mlx *mlx)
+// {
+// 	double	t;
+// 	int		color[4];
+
+// 	t = (double)mlx->iter / (double)mlx->iter_max;
+// 	color[0] = 0;
+// 	if (mlx->shift == 1)
+// 	{
+// 		color[1] = (rand()) * 255 * t;
+// 		color[2] = (rand()) * 1 * t;
+// 		color[3] = (rand()) * 125 * (1 - t);
+// 	}
+// 	if (mlx->shift == 2)
+// 	{
+// 		color[2] = (int)(9 * (1 - t) * pow(t, 3) * 235);
+// 		color[1] = (int)(13 * pow((1 - t), 2) * pow(t, 2) * 255);
+// 		color[3] = (int)(7 * pow((1 - t), 3) * t * 225);
+// 	}
+// 	if (mlx->shift == 3)
+// 	{
+// 		color[3] = (int)(9 * (1 - t) * pow(t, 3) * 255);
+// 		color[2] = (int)(15 * pow((1 - t), 2) * pow(t, 2) * 121);
+// 		color[1] = (int)(8.5 * pow((1 - t), 3) * t * 225);
+// 	}
+// 	return (trgb_gen(color[0], color[1], color[2], color[3]));
+// }
--- src/mouse_oper.c
+++ src/mouse_oper.c
@@ -6,7 +6,7 @@
 /*   By: mabbas <mabbas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 19:23:34 by mabbas            #+#    #+#             */
-/*   Updated: 2022/12/10 00:54:55 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/12/10 22:50:38 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,18 @@
  *  An extra check is there for the coeff value range.
  **/
 
+// double	lerp(double start, double end, double inter_coeff)
+// {
+// 	if (inter_coeff <= 0.0)
+// 		return (start);
+// 	if (inter_coeff >= 1.0)
+// 		return (end);
+// 	return ((1 - inter_coeff) * start + inter_coeff * end);
+// }
+
 double	lerp(double start, double end, double inter_coeff)
 {
-	if (inter_coeff <= 0.0)
-		return (start);
-	if (inter_coeff >= 1.0)
-		return (end);
-	return ((1 - inter_coeff) * start + inter_coeff * end);
+	return (start + ((end - start) * inter_coeff));
 }
 
 int	ctrl_mouse(int key, int x, int y, t_mlx *mlx)
@@ -32,7 +37,6 @@ int	ctrl_mouse(int key, int x, int y, t_mlx *mlx)
 	t_oper	mouse;
 	double	zoom;
 
-	zoom = 0.0;
 	if (key == 1)
 		mlx->press = 1;
 	mouse.r = (mlx->min.r + x * ((mlx->max.r - mlx->min.r)
@@ -40,15 +44,15 @@ int	ctrl_mouse(int key, int x, int y, t_mlx *mlx)
 	mouse.i = (mlx->max.i - y * ((mlx->max.i - mlx->min.i)
 				/ (HEIGHT)));
 	if (key == 5)
-		zoom = 1.10;
+		zoom = 1.20;
 	else if (key == 4)
-		zoom = 0.75;
+		zoom = 0.80;
 	else
-		zoom = 1;
+		zoom = 1.0;
 	mlx->min.r = lerp(mouse.r, mlx->min.r, zoom);
 	mlx->min.i = lerp(mouse.i, mlx->min.i, zoom);
 	mlx->max.r = lerp(mouse.r, mlx->max.r, zoom);
-	mlx->min.i = lerp(mouse.i, mlx->min.r, zoom);
+	mlx->max.i = lerp(mouse.i, mlx->max.i, zoom);
 	render_fractal(mlx);
 	return (0);
 }
@@ -61,7 +65,7 @@ int	julia_key_press(int key, int x, int y, t_mlx *mlx)
 	(void) x;
 	(void) y;
 	if (key == 1)
-		mlx->press = KEY_A;
+		mlx->press = 0;
 	return (0);
 }
 
